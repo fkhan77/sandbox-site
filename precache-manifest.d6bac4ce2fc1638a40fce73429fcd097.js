570d3da4edbcafee52a6672d6bfb29f7fb644e7c
Farhad Khan <farhadk1996@gmail.com>
2020-06-15 23:32:10 +0100

    Updates

---
+++ precache-manifest.d6bac4ce2fc1638a40fce73429fcd097.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69723abe43cbfcb182193fd32ba01fe",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "3e96343384bd4ef09fa2",
+    "url": "/portfolio-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "16862f570fe1057b7dcb",
+    "url": "/portfolio-site/static/js/2.ff075d94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-site/static/js/2.ff075d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e96343384bd4ef09fa2",
+    "url": "/portfolio-site/static/js/main.b0ba9719.chunk.js"
+  },
+  {
+    "revision": "445412cdfc9cbd9c889b",
+    "url": "/portfolio-site/static/js/runtime-main.b7592738.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
