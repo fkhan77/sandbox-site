b3dd1f171ba42ffb4f5611b91cb7aa6f74bfe491
Farhad Khan <farhadk1996@gmail.com>
2020-06-15 23:47:02 +0100

    Remove boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src='https://psmedia.playstation.com/is/image/psmedia/furi-two-column-01-ps4-eu-26oct15?$TwoColumn_Image$'
+          className='App-logo'
+          alt='logo'
+        />
+        <p>Gonna get round to it one day...</p>
       </header>
     </div>
   );
