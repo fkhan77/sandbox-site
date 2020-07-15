8c20b3a3a2e45c8edb7e7eaece85af3c0f41d48b
Farhad Khan <farhadk1996@gmail.com>
2020-07-15 17:26:17 +0100

    Minor style changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { HomePage, Page1, Page2 } from "./Pages";
+import spelunky from "./assets/images/spelunky.png";
 
 function App() {
   const [showSidebar, setShowSidebar] = useState();
@@ -12,11 +13,14 @@ function App() {
         style={{
           animationName: showSidebar,
         }}
-      />
+      >
+        <img src={spelunky} width='100%' />
+      </div>
       <Router>
-        <nav className='App-nav-container'>
+        <header className='header'>
+          <div className='header--background' />
           <svg
-            className='App-nav-menu'
+            className='header-menu'
             z-index='10'
             onClick={() =>
               showSidebar === "sidebar-open"
@@ -31,18 +35,18 @@ function App() {
             <rect y='30' width='100' height='20' rx='8'></rect>
             <rect y='60' width='100' height='20' rx='8'></rect>
           </svg>
-          <div className='App-nav'>
+          <div className='header-nav'>
             <Link to='/' style={{ textDecoration: "none" }}>
-              <div className='App-nav--item'>Farhad Khan</div>
+              <div className='header-nav--item'>Farhad Khan</div>
             </Link>
             <Link to='/page1' style={{ textDecoration: "none" }}>
-              <div className='App-nav--item'>Projects</div>
+              <div className='header-nav--item'>Projects</div>
             </Link>{" "}
             <Link to='/page2' style={{ textDecoration: "none" }}>
-              <div className='App-nav--item'>About</div>
+              <div className='header-nav--item'>About</div>
             </Link>{" "}
           </div>
-        </nav>
+        </header>
 
         <Switch>
           <Route path='/page1'>
